cfd995261a8e18655e434c192a98010eaaeb4ea3
SeNS <sens.boston@gmail.com>
2017-02-09 22:02:35 -0500

    Initial code commit

---
+++ JoystickTest/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MOGAController;
+
+namespace JoystickTest
+{
+    public sealed partial class MainPage : Page
+    {
+        Controller controller = new Controller();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            controller.StateChanged += Controller_StateChanged;
+            controller.KeyChanged += Controller_KeyChanged;
+            controller.AxisChanged += Controller_AxisChanged;
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (sender as Button);
+            if (btn.Content.Equals("Disconnect"))
+            {
+                controller.Disconnect();
+                keyStatus.Text = axisStatus.Text = "";
+                (sender as Button).Content = "Connect to BD&A";
+            }
+            else
+            {
+                controller.Connect();
+                (sender as Button).Content = "Disconnect";
+            }
+        }
+
+        private void Controller_StateChanged(StateEvent param)
+        {
+            controllerStatus.Text = param.StateValue.ToString();
+        }
+
+        private void Controller_KeyChanged(KeyEvent param)
+        {
+            keyStatus.Text = "keycode: " + param.KeyCode.ToString() + "   action: " + param.Action.ToString();
+        }
+
+        private void Controller_AxisChanged(MotionEvent param)
+        {
+            axisStatus.Text = "axis: " + param.Axis.ToString() + "   value: " + param.AxisValue.ToString();
+        }
+    }
+}
---
+++ MOGAController/MOGAController.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Linq;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Devices.Enumeration;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+
+namespace MOGAController
+{
+    public enum ControllerAction
+    {
+        Pressed,
+        Unpressed
+    }
+
+    public enum ControllerState
+    {
+        Connection = 10,
+        PowerLow = 11,
+        SupportedVersion = 12,
+        SelectedVersion = 13
+    }
+
+    public enum ControllerResult
+    {
+        Connected = 0,
+        Disconnected = 1,
+        Connecting = 2,
+        NoMogaDevicePairingFound = 3,
+        VersionMoga = 20,
+        VersionMogaPro = 21
+    }
+
+    public enum Axis
+    {
+        X = 90,
+        Y = 91,
+        Z = 92,
+        RZ = 93,
+        LeftTrigger = 94,
+        RightTrigger = 95
+    }
+
+    public enum KeyCode
+    {
+        A = 10,
+        B = 11,
+        X = 12,
+        Y = 13,
+        Start = 14,
+        Select = 15,
+        L1 = 16,
+        R1 = 17,
+        L2 = 18,
+        R2 = 19,
+        ThumbLeft = 20,
+        ThumbRight = 21,
+        DirectionUp = 22,
+        DirectionDown = 23,
+        DirectionLeft = 24,
+        DirectionRight = 25
+    }
+
+    public delegate void StateChangedEventHandler(StateEvent args);
+    public delegate void KeyEventHandler(KeyEvent args);
+    public delegate void MotionEventHandler(MotionEvent args);
+
+    public sealed class KeyEvent
+    {
+        public ControllerAction Action { get; }
+        public KeyCode KeyCode { get; }
+
+        public KeyEvent(KeyCode keyCode, ControllerAction action) { KeyCode = keyCode; Action = action; }
+    }
+
+    public sealed class MotionEvent
+    {
+        public Axis Axis { get; }
+
+        public float AxisValue { get; }
+
+        public MotionEvent(Axis axis, float axisValue) { Axis = axis; AxisValue = axisValue; }
+    }
+
+    public sealed class StateEvent
+    {
+        public ControllerResult StateValue { get; }
+
+        public StateEvent(ControllerResult stateValue) { StateValue = stateValue; }
+    }
+
+    public sealed class Controller
+    {
+        private DeviceInformation _serviceInfo;
+        private RfcommDeviceService _rfcommService;
+        private StreamSocket _socket;
+        private CancellationTokenSource _cancelationSource;
+
+        private const uint RECVMSG_LEN = 12;
+        private const byte SENDMSG_LEN = 5;
+        private const uint RECVBUF_LEN = 256;
+        private const int  MOGABUF_LEN = 8;
+
+        private const byte CMD_RESET = 65;
+        private const byte CMD_INIT = 67;
+        private const byte CMD_FLUSH = 68;
+
+        private byte[] m_State = new byte[MOGABUF_LEN];
+
+        public bool IsConnected { get; private set; }
+
+        private bool _cancelListener = false;
+        private bool _cancelConnect = false;
+
+        public event StateChangedEventHandler StateChanged;
+
+        public event MotionEventHandler AxisChanged;
+
+        public event KeyEventHandler KeyChanged;
+
+        public void Disconnect()
+        {
+            _cancelConnect = _cancelListener = true;
+        }
+
+        public async void Connect()
+        {
+            _cancelConnect = _cancelListener = IsConnected = false;
+            var serviceInfoCollection = await DeviceInformation.FindAllAsync(RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort));
+            _serviceInfo = serviceInfoCollection.Where(s => s.Name == "BD&A").FirstOrDefault();
+
+            if (_serviceInfo != null)
+            {
+                // Initialize the target Bluetooth RFCOMM device service
+                _rfcommService = await RfcommDeviceService.FromIdAsync(_serviceInfo.Id);
+
+                if (_rfcommService != null)
+                {
+                    StateChanged?.Invoke(new StateEvent(ControllerResult.Connecting));
+
+                    // For connect retrying 
+                    while (!_cancelConnect)
+                    {
+                        try
+                        {
+                            _socket = new StreamSocket();
+                            await _socket.ConnectAsync(_rfcommService.ConnectionHostName, _rfcommService.ConnectionServiceName, SocketProtectionLevel.PlainSocket);
+
+                            IsConnected = true;
+                            StateChanged?.Invoke(new StateEvent(ControllerResult.Connected));
+                            Debug.WriteLine("Connected!");
+
+                            // Set controller ID
+                            await SendMessage(CMD_INIT);
+
+                            await Task.Factory.StartNew(SocketListener, new CancellationTokenSource().Token, TaskCreationOptions.LongRunning, TaskScheduler.FromCurrentSynchronizationContext());
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Debug.WriteLine(error.Message);
+                            await Task.Delay(500);
+                        }
+                    }
+                }
+                else StateChanged?.Invoke(new StateEvent(ControllerResult.NoMogaDevicePairingFound));
+            }
+            else StateChanged?.Invoke(new StateEvent(ControllerResult.NoMogaDevicePairingFound));
+            Debug.WriteLine("exiting Connect()");
+        }
+
+        private async Task SocketListener()
+        {
+            await SendMessage(CMD_RESET);
+            while (!_cancelListener)
+            {
+                await SendMessage(CMD_FLUSH);
+                var retCode = await ReceiveMessage(TimeSpan.FromSeconds(29));
+                if (retCode < 1)
+                {
+                    if (retCode == -3) _cancelListener = true;
+                    else await SendMessage(CMD_RESET);
+                }
+                else ProcessData(); 
+            }
+
+            _socket.Dispose();
+            _socket = null;
+
+            IsConnected = false;
+            StateChanged?.Invoke(new StateEvent(ControllerResult.Disconnected));
+        }
+
+        #region Sending/receiving data
+
+        // Moga MODE A command codes discovered so far:
+        // -Sent TO Moga controller
+        //    65 - poll controller state, digital triggers  (12b response)
+        //    67 - change controller id
+        //    68 - listen mode, digital triggers  (12b response)
+        //    69 - poll controller state, analog triggers  (14b response)
+        //    70 - listen mode, analog triggers  (14b response)
+        // -Recv FROM Moga controller
+        //    97 - poll command response, digital triggers  (12b response)
+        //   100 - listen mode status update, digital triggers  (12b response)
+        //   101 - poll command response, analog triggers  (14b response)
+        //   102 - listen mode status update, analog triggers  (14b response)
+        // Oddly, there seems to be no way to obtain battery status.  It's reported in HID Mode B, but not here.
+        private async Task<int> SendMessage(byte code)
+        {
+            if (_socket == null) return 1;
+            byte i, chksum = 0;
+            byte[] msg = new byte[SENDMSG_LEN];
+
+            msg[0] = 0x5a;            // identifier
+            msg[1] = SENDMSG_LEN;     // message length - always 5
+            msg[2] = code;            // command to send
+            msg[3] = 1;               // controller id
+            // calculate a check sum
+            for (i = 0; i < SENDMSG_LEN - 1; i++)
+                chksum = (byte)(msg[i] ^ chksum);
+            msg[4] = chksum;
+
+            try
+            {
+                await _socket.OutputStream.WriteAsync(msg.AsBuffer());
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Exception on SendMessage:" + error.Message);
+            }
+            return 1;
+        }
+
+        // Received messages are a similar format to the sent messages:
+        //   byte 0 - 0x7a identifier 
+        //   byte 1 - length, 12 or 14
+        //   byte 2 - message code
+        //   byte 3 - controller id
+        //   4 - 9 or 11 - data bytes
+        //   10 or 12    - 0x10 ..not sure what this means.  Could be identifying the kind of Moga.
+        //   11 or 13    - checksum
+        // If the message doesn't validate, something is messed up.  Just reset the connection.
+        private async Task<int> ReceiveMessage(TimeSpan timeout)
+        {
+            if (_socket == null) return -1;
+
+            byte i, chksum = 0;
+            var recvBuf = new byte[RECVBUF_LEN];
+
+            // Returned data can be 12 or 14 bytes long, so the message length should be checked before a full read.
+            // I dislike making assumptions on socket reads, but in the interests of streamlining things as much as possible
+            // to maybe cut down on lag, and since we do know what the length will be, I'll hardcode the recv message length.
+
+            if (_cancelationSource != null) _cancelationSource.Dispose();
+            _cancelationSource = new CancellationTokenSource();
+            _cancelationSource.CancelAfter(timeout);
+
+            try
+            {
+                var newBuffer = await _socket.InputStream.ReadAsync(recvBuf.AsBuffer(), RECVMSG_LEN, InputStreamOptions.Partial).AsTask(_cancelationSource.Token);
+
+                // Incorrect amount of data: socket error or timeout
+                if (newBuffer.Length != RECVMSG_LEN) return -1;
+
+                // Calculate checksum
+                for (i = 0; i < RECVMSG_LEN - 1; i++)
+                    chksum = (byte)(recvBuf[i] ^ chksum);
+
+                // Received bad data
+                if (recvBuf[0] != 0x7a || recvBuf[RECVMSG_LEN - 1] != chksum) return -2;
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine("Exception on ReceiveMessage:" + error.Message);
+                return -3; // timeout
+            }
+
+            // Copy MOGA data
+            Array.Copy(recvBuf, 4, m_State, 0, MOGABUF_LEN);
+            return 1;
+        }
+
+        private byte buttonA = 0;
+        private byte buttonB = 0;
+        private byte buttonX = 0;
+        private byte buttonY = 0;
+        private byte buttonL1 = 0;
+        private byte buttonR1 = 0;
+        private byte buttonSelect = 0;
+        private byte buttonStart = 0;
+
+        private int axisX = 0;
+        private int axisY = 0;
+        private int axisZ = 0;
+        private int axisRZ = 0;
+
+        private sbyte dataX = 0;
+        private sbyte dataY = 0;
+        private sbyte dataZ = 0;
+        private sbyte dataRZ = 0;
+
+        private void ProcessData()
+        {
+            // First, process buttons
+            Debug.WriteLine(BitConverter.ToString(m_State));
+
+            // First, process the buttons
+            byte newButtonA = (byte)((m_State[0] >> 2) & 1);        // A
+            byte newButtonB = (byte)((m_State[0] >> 1) & 1);        // B
+            byte newButtonX = (byte)((m_State[0] >> 3) & 1);        // X
+            byte newButtonY = (byte)((m_State[0] >> 0) & 1);        // Y
+            byte newButtonL1 = (byte)((m_State[0] >> 6) & 1);       // L1
+            byte newButtonR1 = (byte)((m_State[0] >> 7) & 1);       // R1
+            byte newButtonSelect = (byte)((m_State[0] >> 5) & 1);   // Select
+            byte newButtonStart = (byte)((m_State[0] >> 4) & 1);    // Start
+
+            if (buttonA != newButtonA) KeyChanged?.Invoke(new KeyEvent(KeyCode.A, newButtonA > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonB != newButtonB) KeyChanged?.Invoke(new KeyEvent(KeyCode.B, newButtonB > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonX != newButtonX) KeyChanged?.Invoke(new KeyEvent(KeyCode.X, newButtonX > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonY != newButtonY) KeyChanged?.Invoke(new KeyEvent(KeyCode.Y, newButtonY > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonL1 != newButtonL1) KeyChanged?.Invoke(new KeyEvent(KeyCode.L1, newButtonL1 > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonR1 != newButtonR1) KeyChanged?.Invoke(new KeyEvent(KeyCode.R1, newButtonR1 > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonSelect != newButtonSelect) KeyChanged?.Invoke(new KeyEvent(KeyCode.Select, newButtonSelect > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+            if (buttonStart != newButtonStart) KeyChanged?.Invoke(new KeyEvent(KeyCode.Start, newButtonStart > 0 ? ControllerAction.Pressed : ControllerAction.Unpressed));
+
+            buttonA = newButtonA;
+            buttonB = newButtonB;
+            buttonX = newButtonX;
+            buttonY = newButtonY;
+            buttonL1 = newButtonL1;
+            buttonR1 = newButtonR1;
+            buttonSelect = newButtonSelect;
+            buttonStart = newButtonStart;
+
+            // Next, process joystick/d-pad axes
+            int newAxisX = 0;
+            sbyte newDataX = (sbyte)m_State[2];
+            if ( (byte) (m_State[1] & 0x04) == 0x04 || (byte) (m_State[1] & 0x08) == 0x08) newAxisX = (byte) (m_State[1] & 0x04) == 0x04 ? 1 : -1;
+            if (axisX != newAxisX || (newAxisX != 0 && dataX != newDataX) || (axisX != 0 && newAxisX == 0))
+            {
+                AxisChanged?.Invoke(new MotionEvent(Axis.X, newAxisX == 0 ? 0 : newDataX));
+            }
+            axisX = newAxisX;
+            dataX = newDataX;
+
+            int newAxisY = 0;
+            sbyte newDataY = (sbyte)m_State[3];
+            if ((byte) (m_State[1] & 0x01) == 0x01 || (byte) (m_State[1] & 0x02) == 2) newAxisY = (byte) (m_State[1] & 0x01) == 0x01 ? 1 : -1;
+            if (axisY != newAxisY || (newAxisY != 0 && dataY != newDataY) || (axisY != 0 && newAxisY == 0))
+            {
+                AxisChanged?.Invoke(new MotionEvent(Axis.Y, newAxisY == 0 ? 0 : newDataY));
+            }
+            axisY = newAxisY;
+            dataY = newDataY;
+
+            int newAxisZ = 0;
+            sbyte newDataZ = (sbyte)m_State[5];
+            if ( (byte) (m_State[1] & 0x10) == 0x10 || (byte) (m_State[1] & 0x20) == 0x20) newAxisZ = (byte) (m_State[1] & 0x10) == 0x10 ? 1 : -1;
+            if (axisZ != newAxisZ || (newAxisZ != 0 && dataZ != newDataZ) || (axisZ != 0 && newAxisZ == 0))
+            {
+                AxisChanged?.Invoke(new MotionEvent(Axis.Z, newAxisZ == 0 ? 0 : newDataZ));
+            }
+            axisZ = newAxisZ;
+            dataZ = newDataZ;
+
+            int newAxisRZ = 0;
+            sbyte newDataRZ = (sbyte)m_State[4];
+            if ( (byte) (m_State[1] & 0x40) == 0x40 || (byte) (m_State[1] & 0x80) == 0x80) newAxisRZ = (byte) (m_State[1] & 0x40) == 0x40 ? 1 : -1;
+            if (axisRZ != newAxisRZ || (newAxisRZ != 0 && dataRZ != newDataRZ) || (axisRZ != 0 && newAxisRZ == 0))
+            {
+                AxisChanged?.Invoke(new MotionEvent(Axis.RZ, newAxisRZ == 0 ? 0 : newDataRZ));
+            }
+            axisRZ = newAxisRZ;
+            dataRZ = newDataRZ;
+        }
+
+        #endregion 
+    }
+}
